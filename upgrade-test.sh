745ceb248e42bcbfbb4cff5c6898f80a51311021
Hard-Nett <123711748+hard-nett@users.noreply.github.com>
2024-10-24 14:46:29 +0000

    Update upgrade-test.sh

--- upgrade-test.sh
+++ upgrade-test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# owner key name in keyring
+KEY1=
+# owner address of key1
+ADDR1=
+# empty balance addr
+ADDR2=
+
+# Define environment variables
 UPGRADE_VERSION_TAG=v0.18.0
 UPGRADE_VERSION_TITLE=v0.18.0
 export CHAIN_ID=sub-2
@@ -7,19 +15,19 @@ export DAEMON_NAME=bitsongd
 export DAEMON_HOME=$HOME/.bitsongd
 source ~/.profile
 
-# prepare node for test
+# Prepare node for test
 wget -q -O - https://git.io/vQhTU | bash -s -- --remove
 wget -q -O - https://git.io/vQhTU | bash -s -- --version 1.22.4
 
-## install current version 
+## Install current version
 git clone -b v0.17.0 https://github.com/permissionlessweb/go-bitsong/
 cd go-bitsong && make install
 
-## build new version to: build/$DAEMON_NAME
-git checkout v0.18.0 
+## Build new version to: build/$DAEMON_NAME
+git checkout $UPGRADE_VERSION_TAG
 make build
 
-## install cosmovisor 
+## Install cosmovisor
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@latest
 
 # Setup service file
@@ -27,7 +35,7 @@ sudo tee /etc/systemd/system/$DAEMON_NAME.service > /dev/null << EOF
 # setup service
 
 [Unit]
-Description=Bitsong  (cosmovisor)
+Description=Bitsong (cosmovisor)
 After=network-online.target
 
 [Service]
@@ -49,49 +57,99 @@ EOF
 # v1 cosmovisor
 cosmovisor init $HOME/go/bin/$DAEMON_NAME
 
-# add upgrade to cosmovisor 
-cosmovisor add-upgrade v0.18.0 build/bitsongd 
+# Add upgrade to cosmovisor
+cosmovisor add-upgrade $UPGRADE_VERSION_TAG build/$DAEMON_NAME
 
-# start cosmovisor 
+
+## setup testnet environment 
+
+coins="100000000000ubtsg"
+delegate="100000000000ubtsg"
+
+$DAEMON_NAME --chain-id $CHAIN_ID init $CHAIN_ID
+sleep 1
+
+jq ".app_state.crisis.constant_fee.denom = \"ubtsg\" |
+      .app_state.staking.params.bond_denom = \"ubtsg\" |
+      .app_state.merkledrop.params.creation_fee.denom = \"ubtsg\" |
+      .app_state.gov.deposit_params.min_deposit[0].denom = \"ubtsg\" |
+      .app_state.fantoken.params.burn_fee.denom = \"ubtsg\" |
+      .app_state.fantoken.params.issue_fee.denom = \"ubtsg\" |
+      .app_state.fantoken.params.mint_fee.denom = \"ubtsg\"" $CHAINDIR/$CHAIN_ID/config/genesis.json > tmp.json
+
+mv tmp.json $CHAINDIR/$CHAIN_ID/config/genesis.json
+
+$DAEMON_NAME keys add validator $KEYRING --output json > validator_seed.json 2>&1
+sleep 1
+$DAEMON_NAME keys add user $KEYRING --output json > key_seed.json 2>&1
+sleep 1
+$DAEMON_NAME keys add relayer $KEYRING --output json > relayer_seed.json 2>&1
+sleep 1
+$DAEMON_NAME add-genesis-account $($DAEMON_NAME keys $KEYRING show user -a) $coins
+sleep 1
+$DAEMON_NAME add-genesis-account $($DAEMON_NAME keys $KEYRING show validator -a) $coins
+sleep 1
+$DAEMON_NAME add-genesis-account $($DAEMON_NAME keys $KEYRING show relayer -a) $coins
+sleep 1
+$DAEMON_NAME gentx validator $delegate $KEYRING --chain-id $CHAIN_ID
+sleep 1
+$DAEMON_NAME gentx validator $delegate $KEYRING --chain-id $CHAIN_ID
+sleep 1
+$DAEMON_NAME collect-gentxs
+sleep 1
+
+echo "Change settings in config.toml and genesis.json files..."
+sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:'"$RPCPORT"'"#g' $CHAINDIR/$CHAIN_ID/config/config.toml
+sed -i 's#"tcp://0.0.0.0:26656"#"tcp://0.0.0.0:'"$P2PPORT"'"#g' $CHAINDIR/$CHAIN_ID/config/config.toml
+sed -i 's#"localhost:6060"#"localhost:'"$PROFPORT"'"#g' $CHAINDIR/$CHAIN_ID/config/config.toml
+sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' $CHAINDIR/$CHAIN_ID/config/config.toml
+sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' $CHAINDIR/$CHAIN_ID/config/config.toml
+sed -i 's/index_all_keys = false/index_all_keys = true/g' $CHAINDIR/$CHAIN_ID/config/config.toml
+#sed -i 's/enable = false/enable = true/g' $CHAINDIR/$CHAIN_ID/config/app.toml
+#sed -i 's/swagger = false/swagger = true/g' $CHAINDIR/$CHAIN_ID/config/app.toml
+sed -i 's/"voting_period": "172800s"/"voting_period": "120s"/g' $CHAINDIR/$CHAIN_ID/config/genesis.json
+sed -i 's/"stake"/"ubtsg"/g' $CHAINDIR/$CHAIN_ID/config/genesis.json
+
+# Start cosmovisor
 sudo systemctl daemon-reload
 sudo systemctl enable $DAEMON_NAME.service
 sudo systemctl start $DAEMON_NAME.service
 
 sleep 10
 
-## create some data 
-## send 
-$DAEMON_NAME tx bank send $KEY1 $KEY2 123ubtsg --gas auto --gas-adjustment 1.2 --chain-id $CHAIN_ID -y
+## Create some data
+## Send
+$DAEMON_NAME tx bank send $ADDR1 $KEY2 123ubtsg --gas auto --gas-adjustment 1.2 --chain-id $CHAIN_ID -y
 
 sleep 3
-## wasm upload 
+## Wasm upload
 $DAEMON_NAME tx wasm upload cw_template.wasm --from test1 --gas auto --gas-adjustment 1.3 --chain-id $CHAIN_ID -y 
 
 sleep 3
-## instantiate
+## Instantiate
 $DAEMON_NAME tx wasm i 1 '{}' --from test1 --gas auto --gas-adjustment 1.3 --chain-id $CHAIN_ID -y 
 
 sleep 3
-### fantoken 
-$DAEMON_NAME tx fantoken issue  --name="refine" --max-supply="1234567890" --uri="ipfs://..." --from test1  --chain-id test-1 --fees 1000000000ubtsg
+### Fantoken
+$DAEMON_NAME tx fantoken issue  --name="refine" --max-supply="1234567890" --uri="ipfs://..." --from test1  --chain-id $CHAIN_ID --fees 1000000000ubtsg
 
-## gov prop
-$DAEMON_NAME tx gov submit-proposal software-upgrade v0.18.0   --title="v0.18.0" --description="upgrade test" --from test1  --deposit 10000000ubtsg --gas auto --gas-adjustment 1.3 --chain-id $CHAIN_ID --upgrade-height 30 --upgrade-info https://raw.githubusercontent.com/permissionlessweb/networks/refs/heads/master/testnet/upgrades/v0.18.0/cosmovisor.json -y
+## Gov prop
+$DAEMON_NAME tx gov submit-proposal software-upgrade $UPGRADE_VERSION_TAG   --title="$UPGRADE_VERSION_TITLE" --description="upgrade test" --from test1  --deposit 10000000ubtsg --gas auto --gas-adjustment 1.3 --chain-id $CHAIN_ID --upgrade-height 30 --upgrade-info https://raw.githubusercontent.com/permissionlessweb/networks/refs/heads/master/testnet/upgrades/$UPGRADE_VERSION_TAG/cosmovisor.json -y
 
-# wait 1 block 
+# Wait 1 block
 sleep 3
 
-# vote 
-$DAEMON_NAME tx gov vote 1 yes --from test1 --gas auto --gas-adjustment 1.2 -y --chain-id test-1
+# Vote
+$DAEMON_NAME tx gov vote 1 yes --from test1 --gas auto --gas-adjustment 1.2 -y --chain-id $CHAIN_ID
 
 sleep 3 
-# stop  
+# Stop
 sudo systemctl stop $DAEMON_NAME.service
 
-# export  
+# Export
 $DAEMON_NAME export > pre-upgrade-export.json
 
-# start  
+# Start
 sudo systemctl start $DAEMON_NAME.service
 
-# upgrade should now be automatically run 
+# Upgrade should now be automatically run
