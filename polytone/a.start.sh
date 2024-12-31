4349b2c79fdf5ecd15cf89c7e05454cfae38081b
hard-nett <hardnettt@proton.me>
2024-12-31 13:20:01 -0500

    reproduce error

--- polytone/a.start.sh
+++ polytone/a.start.sh
@@ -28,26 +28,25 @@ POLYTONE_CONTRACTS=(
   )
 
 # Define the new ports for val1 on chain a
-VAL1A_API_PORT=1317
-VAL1A_GRPC_PORT=9090
-VAL1A_GRPC_WEB_PORT=9091
-VAL1A_PROXY_APP_PORT=26658
-VAL1A_RPC_PORT=26657
-VAL1A_PPROF_PORT=6060
-VAL1A_P2P_PORT=26656
+VAL1_API_PORT=1317
+VAL1_GRPC_PORT=9090
+VAL1_GRPC_WEB_PORT=9091
+VAL1_PROXY_APP_PORT=26658
+VAL1_RPC_PORT=26657
+VAL1_PPROF_PORT=6060
+VAL1_P2P_PORT=26656
 
 # Define the new ports for val1 on chain b
-VAL1B_API_PORT=1318
-VAL1B_GRPC_PORT=10090
-VAL1B_GRPC_WEB_PORT=10091
-VAL1B_PROXY_APP_PORT=9395
-VAL1B_RPC_PORT=27657
-VAL1B_PPROF_PORT=6361
-VAL1B_P2P_PORT=26356
+VAL2_API_PORT=1318
+VAL2_GRPC_PORT=10090
+VAL2_GRPC_WEB_PORT=10091
+VAL2_PROXY_APP_PORT=9395
+VAL2_RPC_PORT=27657
+VAL2_PPROF_PORT=6361
+VAL2_P2P_PORT=26356
 
-
-echo "Creating $BINARY instance for VAL1_A: home=$VAL1HOME | chain-id=$CHAINID_A | p2p=:$VAL1A_P2P_PORT | rpc=:$VAL1A_RPC_PORT | profiling=:$VAL1A_PPROF_PORT | grpc=:$VAL1A_GRPC_PORT"
-echo "Creating $BINARY instance for VAL: home=$VAL2HOME | chain-id=$CHAINID_B | p2p=:$VAL1B_P2P_PORT | rpc=:$VAL1B_RPC_PORT | profiling=:$VAL1B_PPROF_PORT | grpc=:$VAL1B_GRPC_PORT"
+echo "Creating $BINARY instance for VAL1_A: home=$VAL1HOME | chain-id=$CHAINID_A | p2p=:$VAL1_P2P_PORT | rpc=:$VAL1_RPC_PORT | profiling=:$VAL1_PPROF_PORT | grpc=:$VAL1_GRPC_PORT"
+echo "Creating $BINARY instance for VAL: home=$VAL2HOME | chain-id=$CHAINID_B | p2p=:$VAL2_P2P_PORT | rpc=:$VAL2_RPC_PORT | profiling=:$VAL2_PPROF_PORT | grpc=:$VAL2_GRPC_PORT"
 
 defaultCoins="100000000000ubtsg"  # 100K
 delegate="1000000ubtsg" # 1btsg
@@ -88,8 +87,8 @@ sleep 1
 $BIND --home $VAL1HOME config chain-id $CHAINID_A
 $BIND --home $VAL2HOME config chain-id $CHAINID_B
 sleep 1
-$BIND --home $VAL1HOME config node tcp://localhost:$VAL1A_RPC_PORT
-$BIND --home $VAL2HOME config node tcp:\/\/127.0.0.1:$VAL1B_RPC_PORT
+$BIND --home $VAL1HOME config node tcp://localhost:$VAL1_RPC_PORT
+$BIND --home $VAL2HOME config node tcp:\/\/127.0.0.1:$VAL2_RPC_PORT
 sleep 1
 
 # optimize val1 genesis for testing
@@ -166,29 +165,29 @@ $BIND genesis collect-gentxs --home $VAL2HOME
 
 # app & config modiifications
 # config.toml
-sed -i.bak -e "s/^proxy_app *=.*/proxy_app = \"tcp:\/\/127.0.0.1:$VAL1A_PROXY_APP_PORT\"/g" $VAL1HOME/config/config.toml &&
-sed -i.bak "/^\[rpc\]/,/^\[/ s/laddr.*/laddr = \"tcp:\/\/127.0.0.1:$VAL1A_RPC_PORT\"/" $VAL1HOME/config/config.toml &&
-sed -i.bak "/^\[rpc\]/,/^\[/ s/address.*/address = \"tcp:\/\/127.0.0.1:$VAL1A_RPC_PORT\"/" $VAL1HOME/config/config.toml &&
-sed -i.bak "/^\[p2p\]/,/^\[/ s/laddr.*/laddr = \"tcp:\/\/0.0.0.0:$VAL1A_P2P_PORT\"/" $VAL1HOME/config/config.toml &&
+sed -i.bak -e "s/^proxy_app *=.*/proxy_app = \"tcp:\/\/127.0.0.1:$VAL1_PROXY_APP_PORT\"/g" $VAL1HOME/config/config.toml &&
+sed -i.bak "/^\[rpc\]/,/^\[/ s/laddr.*/laddr = \"tcp:\/\/127.0.0.1:$VAL1_RPC_PORT\"/" $VAL1HOME/config/config.toml &&
+sed -i.bak "/^\[rpc\]/,/^\[/ s/address.*/address = \"tcp:\/\/127.0.0.1:$VAL1_RPC_PORT\"/" $VAL1HOME/config/config.toml &&
+sed -i.bak "/^\[p2p\]/,/^\[/ s/laddr.*/laddr = \"tcp:\/\/0.0.0.0:$VAL1_P2P_PORT\"/" $VAL1HOME/config/config.toml &&
 sed -i.bak -e "s/^grpc_laddr *=.*/grpc_laddr = \"\"/g" $VAL1HOME/config/config.toml &&
 sed -i.bak -e "s/^pprof_laddr *=.*/pprof_laddr = \"localhost:6060\"/g" $VAL1HOME/config/config.toml &&
 # val2
-sed -i.bak -e "s/^proxy_app *=.*/proxy_app = \"tcp:\/\/127.0.0.1:$VAL1B_PROXY_APP_PORT\"/g" $VAL2HOME/config/config.toml &&
-sed -i.bak "/^\[rpc\]/,/^\[/ s/laddr.*/laddr = \"tcp:\/\/127.0.0.1:$VAL1B_RPC_PORT\"/" $VAL2HOME/config/config.toml &&
-sed -i.bak "/^\[rpc\]/,/^\[/ s/address.*/address = \"tcp:\/\/127.0.0.1:$VAL1B_RPC_PORT\"/" $VAL2HOME/config/config.toml &&
-sed -i.bak "/^\[p2p\]/,/^\[/ s/laddr.*/laddr = \"tcp:\/\/0.0.0.0:$VAL1B_P2P_PORT\"/" $VAL2HOME/config/config.toml &&
+sed -i.bak -e "s/^proxy_app *=.*/proxy_app = \"tcp:\/\/127.0.0.1:$VAL2_PROXY_APP_PORT\"/g" $VAL2HOME/config/config.toml &&
+sed -i.bak "/^\[rpc\]/,/^\[/ s/laddr.*/laddr = \"tcp:\/\/127.0.0.1:$VAL2_RPC_PORT\"/" $VAL2HOME/config/config.toml &&
+sed -i.bak "/^\[rpc\]/,/^\[/ s/address.*/address = \"tcp:\/\/127.0.0.1:$VAL2_RPC_PORT\"/" $VAL2HOME/config/config.toml &&
+sed -i.bak "/^\[p2p\]/,/^\[/ s/laddr.*/laddr = \"tcp:\/\/0.0.0.0:$VAL2_P2P_PORT\"/" $VAL2HOME/config/config.toml &&
 sed -i.bak -e "s/^grpc_laddr *=.*/grpc_laddr = \"\"/g" $VAL2HOME/config/config.toml &&
 sed -i.bak -e "s/^pprof_laddr *=.*/pprof_laddr = \"localhost:6070\"/g" $VAL2HOME/config/config.toml &&
 # app.toml
 sed -i.bak "/^\[api\]/,/^\[/ s/minimum-gas-prices.*/minimum-gas-prices = \"0.0ubtsg\"/" $VAL1HOME/config/app.toml &&
-sed -i.bak "/^\[api\]/,/^\[/ s/address.*/address = \"tcp:\/\/0.0.0.0:$VAL1A_API_PORT\"/" $VAL1HOME/config/app.toml &&
-sed -i.bak "/^\[grpc\]/,/^\[/ s/address.*/address = \"localhost:$VAL1A_GRPC_PORT\"/" $VAL1HOME/config/app.toml &&
-sed -i.bak "/^\[grpc-web\]/,/^\[/ s/address.*/address = \"localhost:$VAL1A_GRPC_WEB_PORT\"/" $VAL1HOME/config/app.toml &&
+sed -i.bak "/^\[api\]/,/^\[/ s/address.*/address = \"tcp:\/\/0.0.0.0:$VAL1_API_PORT\"/" $VAL1HOME/config/app.toml &&
+sed -i.bak "/^\[grpc\]/,/^\[/ s/address.*/address = \"localhost:$VAL1_GRPC_PORT\"/" $VAL1HOME/config/app.toml &&
+sed -i.bak "/^\[grpc-web\]/,/^\[/ s/address.*/address = \"localhost:$VAL1_GRPC_WEB_PORT\"/" $VAL1HOME/config/app.toml &&
 # val2
 sed -i.bak "/^\[api\]/,/^\[/ s/minimum-gas-prices.*/minimum-gas-prices = \"0.0ubtsg\"/" $VAL2HOME/config/app.toml &&
-sed -i.bak "/^\[api\]/,/^\[/ s/address.*/address = \"tcp:\/\/0.0.0.0:$VAL1B_API_PORT\"/" $VAL2HOME/config/app.toml &&
-sed -i.bak "/^\[grpc\]/,/^\[/ s/address.*/address = \"localhost:$VAL1B_GRPC_PORT\"/" $VAL2HOME/config/app.toml &&
-sed -i.bak "/^\[grpc-web\]/,/^\[/ s/address.*/address = \"localhost:$VAL1B_GRPC_WEB_PORT\"/" $VAL2HOME/config/app.toml &&
+sed -i.bak "/^\[api\]/,/^\[/ s/address.*/address = \"tcp:\/\/0.0.0.0:$VAL2_API_PORT\"/" $VAL2HOME/config/app.toml &&
+sed -i.bak "/^\[grpc\]/,/^\[/ s/address.*/address = \"localhost:$VAL2_GRPC_PORT\"/" $VAL2HOME/config/app.toml &&
+sed -i.bak "/^\[grpc-web\]/,/^\[/ s/address.*/address = \"localhost:$VAL2_GRPC_WEB_PORT\"/" $VAL2HOME/config/app.toml &&
 
 
 # Start chains
@@ -215,7 +214,8 @@ if [ -z "$(ls -A ./bin)" ]; then
   sh download.sh
   while [ -z "$(ls -A ./bin)" ]; do
     sleep 1
-done
+  done
+fi
 
 ## upload polytone 
   for contract in "${POLYTONE_CONTRACTS[@]}"; do
@@ -249,18 +249,18 @@ cp ../pfm/hermes.toml $HERMES/config.toml
 ## modify $HERMES_CFG toml with correct values 
 sed -i.bak "/^\[chains\]/,/^\[/ { 
     /id = \"$CHAINID_A\"/ { 
-        s/rpc_addr.*/rpc_addr = \"http:\/\/127.0.0.1:$VAL1A_RPC_PORT\"/; 
-        s/grpc_addr.*/grpc_addr = \"http:\/\/127.0.0.1:$VAL1A_GRPC_PORT\"/; 
-        s/event_source.url.*/event_source.url = \"ws:\/\/127.0.0.1:$VAL1A_RPC_PORT\/websocket\"/; 
+        s/rpc_addr.*/rpc_addr = \"http:\/\/127.0.0.1:$VAL1_RPC_PORT\"/; 
+        s/grpc_addr.*/grpc_addr = \"http:\/\/127.0.0.1:$VAL1_GRPC_PORT\"/; 
+        s/event_source.url.*/event_source.url = \"ws:\/\/127.0.0.1:$VAL1_RPC_PORT\/websocket\"/; 
         s/key_name.*/key_name = \"$VAL\"/; 
     } 
 }" "$HERMES/config.toml"
 
 sed -i.bak "/^\[chains\]/,/^\[/ { 
     /id = \"$CHAINID_B\"/ { 
-        s/rpc_addr.*/rpc_addr = \"http:\/\/127.0.0.1:$VAL1B_RPC_PORT\"/; 
-        s/grpc_addr.*/grpc_addr = \"http:\/\/127.0.0.1:$VAL1B_GRPC_PORT\"/; 
-        s/event_source.url.*/event_source.url = \"ws:\/\/127.0.0.1:$VAL1B_RPC_PORT\/websocket\"/; 
+        s/rpc_addr.*/rpc_addr = \"http:\/\/127.0.0.1:$VAL2_RPC_PORT\"/; 
+        s/grpc_addr.*/grpc_addr = \"http:\/\/127.0.0.1:$VAL2_GRPC_PORT\"/; 
+        s/event_source.url.*/event_source.url = \"ws:\/\/127.0.0.1:$VAL2_RPC_PORT\/websocket\"/; 
         s/key_name.*/key_name = \"$VAL\"/; 
     } 
 }" "$HERMES/config.toml"
@@ -286,14 +286,12 @@ POLYONE_VOICE_ID=4
 POLYONE_TESTER_ID=5
 
 # init note
- $BIND tx wasm i $POLYONE_NOTE_ID '{"block_max_gas": 100000000 }' --from $DEL --home $VAL1HOME --chain-id $CHAINID_A --no-admin --label="note contract chain1" --fees 400000ubtsg --gas auto --gas-adjustment 1.3 -y 
- sleep 1 
- $BIND tx wasm i $POLYONE_NOTE_ID '{"block_max_gas": 100000000 }' --from $USER --home $VAL2HOME --chain-id $CHAINID_B --no-admin --label="note contract chain2" --fees 400000ubtsg --gas auto --gas-adjustment 1.3 -y
- sleep 5 
+ $BIND tx wasm i $POLYONE_NOTE_ID '{"block_max_gas": "100000000" }' --from $DEL --home $VAL1HOME --chain-id $CHAINID_A --no-admin --label="note contract chain1" --fees 400000ubtsg --gas auto --gas-adjustment 1.3 -y 
+ $BIND tx wasm i $POLYONE_NOTE_ID '{"block_max_gas": "100000000" }' --from $USER --home $VAL2HOME --chain-id $CHAINID_B --no-admin --label="note contract chain2" --fees 400000ubtsg --gas auto --gas-adjustment 1.3 -y
+  sleep 5
 # init voice
- $BIND tx wasm i $POLYONE_VOICE_ID '{"proxy_code_id":'$POLYONE_PROXY_ID',"block_max_gas":100000000, "contract_addr_len":32}' --from $DEL --home $VAL1HOME --chain-id $CHAINID_A -y --fees 400000ubtsg --gas auto --gas-adjustment 1.3
-  sleep 1 
- $BIND tx wasm i $POLYONE_VOICE_ID '{"proxy_code_id":'$POLYONE_PROXY_ID',"block_max_gas":100000000, "contract_addr_len":32}'  --from $DEL --home $VAL2HOME --chain-id $CHAINID_B -y --fees 400000ubtsg --gas auto --gas-adjustment 1.3
+ $BIND tx wasm i $POLYONE_VOICE_ID '{"proxy_code_id":"3","block_max_gas":"100000000" }' --from $DEL --home $VAL1HOME --chain-id $CHAINID_A --no-admin --label="voice contract chain1" -y --fees 400000ubtsg --gas auto --gas-adjustment 1.3
+ $BIND tx wasm i $POLYONE_VOICE_ID '{"proxy_code_id":"3","block_max_gas":"100000000" }' --from $DEL --home $VAL2HOME --chain-id $CHAINID_B --no-admin --label="voice contract chain1" -y --fees 400000ubtsg --gas auto --gas-adjustment 1.3
  sleep 5
 # init tester
  $BIND tx wasm i $POLYONE_TESTER_ID '{}' --from $DEL --home $VAL1HOME --no-admin --label="tester contract chain1" --fees 400000ubtsg --gas auto --gas-adjustment 1.3 -y 
@@ -309,15 +307,24 @@ POLYONE_TESTER_ADDR_A=$($BIND q wasm lca $POLYONE_TESTER_ID --home $VAL1HOME -o 
 POLYONE_TESTER_ADDR_B=$($BIND q wasm lca $POLYONE_TESTER_ID --home $VAL2HOME -o json | jq -r .contracts[0])
 POLYONE_VOICE_ADDR_A=$($BIND q wasm lca $POLYONE_VOICE_ID  --home $VAL1HOME -o json | jq -r .contracts[0])
 POLYONE_VOICE_ADDR_B=$($BIND q wasm lca $POLYONE_VOICE_ID  --home $VAL2HOME -o json | jq -r .contracts[0])
-
+echo "POLYONE_PROXY_ADDR_A: $POLYONE_PROXY_ADDR_A"
+echo "POLYONE_PROXY_ADDR_B: $POLYONE_PROXY_ADDR_B"
+echo "POLYONE_NOTE_ADDR_A: $POLYONE_NOTE_ADDR_A"
+echo "POLYONE_NOTE_ADDR_B: $POLYONE_NOTE_ADDR_B"
+echo "POLYONE_TESTER_ADDR_A: $POLYONE_TESTER_ADDR_A"
+echo "POLYONE_TESTER_ADDR_B: $POLYONE_TESTER_ADDR_B"
+echo "POLYONE_VOICE_ADDR_A: $POLYONE_VOICE_ADDR_A"
+echo "POLYONE_VOICE_ADDR_B: $POLYONE_VOICE_ADDR_B"
 ## create channel 
 echo "starting relayer" 
 echo "Creating IBC transfer channel"
-hermes create channel --a-chain $CHAINID_A -b-chain $CHAINID_B\
+hermes create channel --a-chain $CHAINID_A --b-chain $CHAINID_B\
     --a-port "wasm.$POLYONE_NOTE_ADDR_A"\
     --b-port "wasm.$POLYONE_VOICE_ADDR_A"\
     --order unordered\
-    --version polytone-1
+    --chan-version polytone-1\
+    --new-client-connection\
+    --yes
 
 # wait for channel to be created
 # hermes create channel --a-chain $CHAINID_A --b-chain $CHAINID_B --a-port transfer --b-port transfer --new-client-connection --yes
@@ -330,7 +337,8 @@ hermes create channel --a-chain $CHAINID_A -b-chain $CHAINID_B\
 ####################################################################
 
 # send msg to note
-$BIND tx wasm e $POLYONE_NOTE_ADDR_A '{"execute":{"msgs":[],"timeout_seconds":100,"callback": {"receiver": '"$POLYONE_TESTER_ADDR"', "msg":"aGVsbG8K"}}}'
+$BIND tx wasm e $POLYONE_NOTE_ADDR_A '{"execute":{"msgs":[],"timeout_seconds":100,"callback": {"receiver": '"$POLYONE_TESTER_ADDR_A"', "msg":"aGVsbG8K"}}}'--home $VAL1HOME --from $DEL -y --fees 400000ubtsg --gas auto --gas-adjustment 1.3
+$BIND tx wasm e $POLYONE_NOTE_ADDR_B '{"execute":{"msgs":[],"timeout_seconds":100,"callback": {"receiver": '"$POLYONE_NOTE_ADDR_B"', "msg":"aGVsbG8K"}}}'--home $VAL2HOME --from $DEL -y --fees 400000ubtsg --gas auto --gas-adjustment 1.3
 
 # wait for packet to relay.
 sleep 60
